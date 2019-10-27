b21b9dc1bfb1db01c139c0d0a210ac32aa90bc3d
Pawel Gasiorowski <gasior002@gmail.com>
2019-10-27 23:10:46 +0100

    Updates

---
+++ precache-manifest.929235fdd378b992fe13c3797cc1013e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc4934d46074b80e2ca80a01140af63",
+    "url": "/recruitment-task/index.html"
+  },
+  {
+    "revision": "e84c8b83dccb0d2a8b3a",
+    "url": "/recruitment-task/static/css/main.8bfae92e.chunk.css"
+  },
+  {
+    "revision": "7b69f571b93f7ded0ad6",
+    "url": "/recruitment-task/static/js/2.57dfc943.chunk.js"
+  },
+  {
+    "revision": "e84c8b83dccb0d2a8b3a",
+    "url": "/recruitment-task/static/js/main.2397ba33.chunk.js"
+  },
+  {
+    "revision": "280e97813bf587f623a4",
+    "url": "/recruitment-task/static/js/runtime-main.53c83b9b.js"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/recruitment-task/static/media/ARROW_CLOSE.81e948ad.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/recruitment-task/static/media/ARROW_OPEN.da45c4c0.svg"
+  },
+  {
+    "revision": "de203a0d51d11479f802bb3e7564abd5",
+    "url": "/recruitment-task/static/media/CROSS.de203a0d.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/recruitment-task/static/media/LOGO.79977f05.svg"
+  }
+]);
